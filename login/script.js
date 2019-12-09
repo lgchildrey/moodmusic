c65c50e0c0066f1ffe3485ce0917c263dddd0f24
Hannah <hannahru@live.unc.edu>
2019-12-08 23:08:03 -0500

    working

--- login/script.js
+++ login/script.js
@@ -1,50 +1,35 @@
-import axios from 'axios';
-
-const pubRoot = new axios.create({
-  baseURL: "http://localhost:3000/public"
-});
-
-async function createAuthor({first = $(), last = 'Doe', numBooks = 0}) {
-  return await pubRoot.post(`/authors/`, {
-    data: {first, last, numBooks}
-  })
-}
-
-(async () => {
-  await createAuthor({
-    first: "chris",
-    numBooks: 4
-  });
-
-  let {data} = await getAllAuthors();
-  console.log(data)
-})();
-$(function() {
-    const $form = $('#login-form');
+const handleSubmitButtonPress = function(event) {
+    event.preventDefault();
+    event.stopImmediatePropagation();
     const $message = $('#message');
-  
-    $form.submit(function(e) {
-      e.preventDefault();
-  
-      $message.html('');
-  
-      const data = $form.serializeArray().reduce((o, x) => {
-        o[x.name] = x.value;
-        return o;
-      }, {});
-      
-      $.ajax({
-        url: 'http://localhost:3000/account/login',
-        type: 'POST',
-        data,
-        xhrFields: {
-            withCredentials: true,
-        },
-      }).then(() => {
-        $message.html('<span class="has-text-success">Success! You are now logged in.</span>');
-        location.href = `../index.html`;
-      }).catch(() => {
+    $message.html('');
+
+    async function loginUser() {
+      try {
+        const result = await axios({
+          method: 'POST',
+          url: 'http://localhost:3000/account/login',
+          data: {
+            name: $('#username').val(), 
+            pass: $('#password').val()
+          }
+       });
+
+       localStorage.setItem('jwt', result.data.jwt);
+       $message.html('<span class="has-text-success">Success! You are now logged in.</span>');
+       location.href = `../index.html`;
+      } catch(error) {
         $message.html('<span class="has-text-danger">Something went wrong. Please check your email and password.</span>');
-      });
-    });
-  });
+      }
+    }
+    loginUser();
+};
+
+const loadIntoDOM = function() {
+  const $root = $('#login');
+  $root.on('click', '#submit', handleSubmitButtonPress);
+};
+
+$(function() {
+    loadIntoDOM();
+});
