bac8dfd7e28c5ef96c1e0847af9f6050f4a2abbf
Hannah <42622670+hannah47@users.noreply.github.com>
2019-12-12 00:50:13 -0500

    almost done

--- signup/render.js
+++ signup/render.js
@@ -26,9 +26,34 @@ const handleCreateButtonPress = function(event) {
        });
 
        localStorage.setItem('jwt', result.data.jwt);
-    }
-
-    createUser();
+    
+   // createUser();
+//   var key = result.data.jwt;
+//   async function getProfile() {
+//       const result1 = await axios({
+//           method: 'GET',
+//           url: 'http://localhost:3000/account/status',
+//           headers: { Authorization: `Bearer ${key}`}
+//       });
+//       return result1;
+//   }
+//   //async function postToUser() {
+//       let profile = await getProfile();
+//       await axios({
+//           method: 'POST',
+//           url: 'http://localhost:3000/user/person',
+//           headers: { Authorization: `Bearer ${key}`},
+//           data: {
+//               name: profile.data.user.name,
+//               data: {
+//                   email: profile.data.user.data.email,
+//               }
+//           }
+//       });
+//  // }
+  }
+  //postToUser();
+  createUser();
     // $message.html('<span class="has-text-success">Success! You have an account now.</span>');
     location.href = `../index.html`;
 };
